d56e2f06a229efccc65b29b0a1fb5e9ad70d068a
Hpar7 <harshadv@buffalo.edu>
2019-12-29 02:51:38 -0500

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var car = {
+    make : 'VW',
+    type : 'Polo',
+    color : 'blue',
+    isTurnedOn: false,
+    numberOfwheels : 4,
+    seats : [
+        'seat 1',
+        'seat 2',
+        'seat 3',
+        'seat 4'
+    ],
+    turnOn : function(){
+        this.isTurnedOn = true;
+    }
+
+    switchCar : function(isOn){
+        console.log('You chose to turn the car ' + isOn)
+
+        if(isOn == true){
+            this.isTurnedOn = true;
+        else{
+            this.isTurnedOn = false;
+        }
+        }
+    }
+
+};
+console.log("Hello Friends");
